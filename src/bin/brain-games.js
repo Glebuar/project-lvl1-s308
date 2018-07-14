510509daa7b4299ac57dba640890f332ad9c34c0
Glebuar <glebuar@gmail.com>
2018-07-14 18:00:31 +0300

    adding files

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,4 +1,9 @@
 #!/usr/bin/env node
 
+import readlineSync from 'readline-sync';
 import userName from '..';
-console.log('Welcome to the Brain Games!');
+
+console.log('Welcome to the Brain Games!\n');
+
+readlineSync.question('May I have your name?\n');
+console.log(`Hello, ${userName}!\n`);
--- src/index.js
+++ src/index.js
@@ -1,3 +1,1 @@
-import readlineSync from 'readline-sync'
-const userName = readlineSync.question('May I have your name?\n');
-console.log(`Hello, ${userName}!\n`);
+
